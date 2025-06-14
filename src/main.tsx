246e12dee50c561be5dd8d25b3bf7af2a309d64e
Your Name <20ins05@gmail.com>
2025-06-15 03:21:43 +0400

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ import { ModalsProvider } from '@mantine/modals';
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const router = createBrowserRouter([
   {
@@ -73,10 +73,9 @@ createRoot(document.getElementById('root')!).render(
       <MantineProvider>
         <ModalsProvider>
           <Notifications />
-          <ConvexProvider client={convex}> 
-            <RouterProvider router={router} />
-          </ConvexProvider>
-        </ModalsProvider>
+          <ConvexProvider client={convex}> <RouterProvider router={router} />
+          </ConvexProvider> 
+          </ModalsProvider>
       </MantineProvider>
     </DirectionProvider>
   </StrictMode>,
